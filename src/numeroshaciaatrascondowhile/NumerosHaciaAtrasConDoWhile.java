6e3715aa8446b5f9ff037ebd44a0f3f25d1547b5
Amarico <Amarico@equipo-zalman>
2018-09-10 19:58:22 +0200

    Creo repositorio

---
+++ src/numeroshaciaatrascondowhile/NumerosHaciaAtrasConDoWhile.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numeroshaciaatrascondowhile;
+
+/**
+ *
+ * @author Amarico
+ */
+public class NumerosHaciaAtrasConDoWhile {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+    
+    //Muestra los números del 320 al 160, contando de 20 en 20 
+    //utilizando un bucle do-while
+    
+    
+  }
+  
+}
